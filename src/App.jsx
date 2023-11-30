94430912395d86a4fb76833da4abde86bd6a9576
Roberto <roberto.quadraccia90@gmail.com>
2023-11-30 18:28:53 +0100

    Props passed part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "../src/components/Header";
 import CoreConcept from "../src/components/CoreConcept";
-import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 const App = () => {
   return (
@@ -10,14 +10,20 @@ const App = () => {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+
+            {/* 
+            We can either use the spread operator above to pass the entire first [0] object in the CORE CONCEPTS ARRAY,
+            or we can pass each individual data like we did below
+            */}
+
             <CoreConcept
-              title="Components"
-              description="The core UI building block."
-              image={componentsImg}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
